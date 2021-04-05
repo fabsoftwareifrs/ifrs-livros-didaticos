f75ebea09a7d85e65ecd78a16d2ebf531e7b1435
= <=>
2021-04-04 21:33:19 -0300

    add playground

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with Foobar.  If not, see <https://www.gnu.org/licenses/>
  */
@@ -44,7 +44,7 @@ const server = new ApolloServer({
     return { authenticatedUser }
   },
   introspection: true,
-  playground: process.env.NODE_ENV !== 'production',
+  playground: process.env.NODE_ENV !== 'production' || true,
   debug: false,
   formatError: (error) => {
     // const e = JSON.stringify(error)
