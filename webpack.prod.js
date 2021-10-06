745f59f0e50d0ee2536f4b7d210a8c470a0ffde8
Eduardo Faggion <>
2021-10-06 14:49:50 -0300

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,12 @@
-const merge = require('webpack-merge')
-const common = require('./webpack.common.js')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const nodeExternals = require('webpack-node-externals')
-
+const webpack = require("webpack");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const nodeExternals = require("webpack-node-externals");
+//devtool: "source-map",
 module.exports = merge(common, {
-  mode: 'production',
-  externals: [nodeExternals({})],
-  plugins: [new CleanWebpackPlugin()],
-})
+  mode: "development",
+  watch: true,
+  externals: [nodeExternals({ allowlist: ["webpack/hot/poll?1000"] })],
+  plugins: [new CleanWebpackPlugin(), new webpack.HotModuleReplacementPlugin()],
+});
